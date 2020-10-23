9a5abf6927494a6a1443ce40b8947c68a616168b
Nuno Morais <nm.morais@campus.fct.unl.pt>
2020-10-23 16:25:51 +0100

    change package name

--- exporter.go
+++ exporter.go
@@ -4,7 +4,7 @@ import (
 	"io"
 	"time"
 
-	"github.com/nm-morais/demmon-common/metrics"
+	"github.com/nm-morais/demmon-common/exporters"
 	"github.com/nm-morais/go-babel/pkg/peer"
 	"github.com/nm-morais/go-babel/pkg/protocol"
 	"github.com/nm-morais/go-babel/pkg/protocolManager"
@@ -20,13 +20,13 @@ type ExporterConf struct {
 type Exporter struct {
 	proto protocol.Protocol
 	confs ExporterConf
-	set   *metrics.Set
+	set   *exporters.Set
 }
 
 func New(confs ExporterConf, babel protocolManager.ProtocolManager) *Exporter {
 	e := &Exporter{
 		confs: confs,
-		set:   metrics.NewSet(),
+		set:   exporters.NewSet(),
 	}
 	e.proto = NewExporterProto(confs, e, babel)
 	return e
@@ -38,17 +38,17 @@ func (e *Exporter) Proto() protocol.Protocol {
 }
 
 // Proto returns the babel proto of the exporter.
-func (e *Exporter) NewCounter(name string) *metrics.Counter {
+func (e *Exporter) NewCounter(name string) *exporters.Counter {
 	return e.set.NewCounter(name)
 }
 
 // Proto returns the babel proto of the exporter.
-func (e *Exporter) NewGauge(name string, f func() float64) *metrics.Gauge {
+func (e *Exporter) NewGauge(name string, f func() float64) *exporters.Gauge {
 	return e.set.NewGauge(name, f)
 }
 
 // Proto returns the babel proto of the exporter.
-func (e *Exporter) NewHistogram(name string) *metrics.Histogram {
+func (e *Exporter) NewHistogram(name string) *exporters.Histogram {
 	return e.set.NewHistogram(name)
 }
 
